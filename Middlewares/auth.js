30e252fe17d4dab598daa87e72e6312cb0cf8f86
Divyansh Singh <singhdivyansh555@gmail.com>
2023-03-05 10:31:20 +0530

    first commit

---
+++ Middlewares/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const auth = async(req,res,next)=>{
+    try {
+        const token = req.headers.authorization;
+        let decodedData = jwt.verify(token,process.env.SECRET);
+        req.userId = decodedData?.id;
+        next();
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
+
+module.exports = auth;
